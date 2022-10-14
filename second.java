a35163328dfc0ac8bd0951c43051cb86eb4d007b
Jawadullahmagsi <115404306+Jawadullahmagsi@users.noreply.github.com>
2022-10-13 22:36:29 -0700

    Add files via upload

---
+++ second.java
@@ -0,0 +1,21 @@
+//Jawadullah 
+//2k22/cse/49
+
+
+import java.util.Scanner;
+class second{
+public static void main(String[] org)
+{
+int number;
+
+Scanner obj=new Scanner(System.in);
+System.out.println("Enter a number to check weather it is even or odd");
+num=obj.nextInt();
+if(number%2==0){
+System.out.println("Number is even ");
+}
+else{
+System.out.println("Number is odd ");
+}
+}
+}
